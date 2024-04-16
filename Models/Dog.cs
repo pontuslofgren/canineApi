bd2192352cbacb0189119669d55035efec571bd2
Pontus <pontus.loefgren@gmail.com>
2024-04-16 16:24:57 +0200

    Add Model

---
+++ Models/Dog.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace canineApi.Models;
+
+public class Dog
+{
+    [Key]
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
