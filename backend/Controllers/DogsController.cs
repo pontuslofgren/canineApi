7df78b3fd202477600f9a518a7deba606e5f9967
Zerophymyr <Zzer0ph@gmail>
2024-04-17 20:40:50 +0200

    config: controller config

--- backend/Controllers/DogsController.cs
+++ backend/Controllers/DogsController.cs
@@ -3,6 +3,7 @@ using Azure.Storage.Blobs.Models;
 using canineApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors
 
 namespace canineApi.Controllers;
 
@@ -27,6 +28,7 @@ public class DogsController : ControllerBase
  }
 
  [HttpPost]
+ [EnableCors("OpenPolicy")]
  public async Task<ActionResult<Dog>> Post([FromForm] DogRequest request)
  {
     var blobServiceClient = new BlobServiceClient(_blobConnectionString);
@@ -52,7 +54,7 @@ public class DogsController : ControllerBase
 
    await _context.Dogs.AddAsync(newDog);
    await _context.SaveChangesAsync();
-    return Ok(newDog);
+   return Ok(newDog);
  }
 
 }
