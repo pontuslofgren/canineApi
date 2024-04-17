370a116d0b747aaff5bb63ce46d4bdc397f26c76
luoxinnan <luoxinnan1@gmail.com>
2024-04-17 13:44:01 +0200

    Display dogs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,39 @@
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient } from "react-query";
+
+
+
 
 function App() {
 
+
+  const queryClient = useQueryClient();
+  const query = useQuery({queryKey:['getdogs'], queryFn: getDogs})
+  
+  async function getDogs(): Promise<Dog[]>{
+    const response = await fetch("https://canineapplab.azurewebsites.net/dogs");
+    const responseJson = await response.json() as Dog[];
+    return responseJson;
+  }
+
+
+  
+
   return (
     <>
-    <h1>hello guys</h1>
+      {query.data?.map((dog) => {
+        <p>{dog.name}</p>
+      })}
+
+      <h1>hello guys</h1>
+
     </>
   )
 }
 
 export default App
+
+type Dog = {
+  id: number;
+  name: string;
+}
