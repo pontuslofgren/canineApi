bae299c473efc26196d934af1d32e19c1f5c5c85
Pontus <pontus.loefgren@gmail.com>
2024-04-17 16:35:26 +0200

    Adds migrations for DogImageUrl

---
+++ backend/Migrations/20240417143402_AddsImageUrlToDog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace canineApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddsImageUrlToDog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Dogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Dogs");
+        }
+    }
+}
