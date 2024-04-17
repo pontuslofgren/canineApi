769f00a2edf31b82bc62e47bdad669091bd42b97
luoxinnan <luoxinnan1@gmail.com>
2024-04-17 14:30:19 +0200

    Add dog form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 
-import {  useQuery} from "react-query";
+import {  useMutation, useQuery, useQueryClient} from "react-query";
+import { useForm, SubmitHandler } from "react-hook-form"
 
 
 
@@ -7,8 +8,15 @@ import {  useQuery} from "react-query";
 function App() {
 
 
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
   const query = useQuery({queryKey:['getdogs'], queryFn: getDogs})
+  
+  const mutation = useMutation({
+    mutationFn: postDog, 
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['getdogs']})
+    }
+  })
   
   async function getDogs(): Promise<Dog[]>{
     const response = await fetch("https://canineapplab.azurewebsites.net/dogs");
@@ -16,15 +24,37 @@ function App() {
     return responseJson;
   }
 
+  async function postDog(name: string){
+    await fetch("https://canineapplab.azurewebsites.net/dogs", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({name: name})
+    });
+    
+  }
+
+  const {
+    register,
+    handleSubmit,
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    mutation.mutate(data.name);
+  }
+
 
   if (query.isLoading) return (<p>Loading...</p>)
   if (query.error) return (<p>Something went wrong.</p>)
   return (
     <>
+<form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="Dog name" {...register("name")} />
+      <input type="submit" />
+    </form>
       {query.data?.map((dog) => {
-        return(<p>{dog.name}</p>)
+        return(
+        <p>{dog.name}</p>
+      )
       })}
-
     </>
   )
 
@@ -35,4 +65,8 @@ export default App
 type Dog = {
   id: number;
   name: string;
+}
+
+type Inputs = {
+  name: string
 }
