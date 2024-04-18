99a3af26b0e010f0b1f8e96c3645c0eb4e94e54f
luoxinnan <luoxinnan1@gmail.com>
2024-04-18 13:39:34 +0200

    Format

--- backend/Controllers/DogsController.cs
+++ backend/Controllers/DogsController.cs
@@ -11,67 +11,68 @@ namespace canineApi.Controllers;
 public class DogsController : ControllerBase
 {
 
-    private readonly CanineContext _context;
-    private readonly string _blobConnectionString;
-    private readonly BlobServiceClient _blobServiceClient;
+   private readonly CanineContext _context;
+   private readonly string _blobConnectionString;
+   private readonly BlobServiceClient _blobServiceClient;
 
-    private readonly BlobContainerClient _containerClient;
+   private readonly BlobContainerClient _containerClient;
 
-    public DogsController(IConfiguration config, CanineContext context)
-    {
-        _context = context;
-        _blobConnectionString = config.GetConnectionString("BlobStorage")!;
-        _blobServiceClient = new BlobServiceClient(_blobConnectionString);
-        _containerClient = _blobServiceClient.GetBlobContainerClient("dogblobs");
-
-    }
- [HttpGet]
- public async Task<ActionResult<List<Dog>>> Get()
- {
-    var dogs = await _context.Dogs.ToListAsync();
-    return Ok(dogs);
- }
-
- [HttpPost]
- public async Task<ActionResult<Dog>> Post([FromForm] DogRequest request)
- {
-   // Set filename for blob
-   string fileName = request.Name + "_" + Guid.NewGuid().ToString();
-// Get a reference to a blob
-   var blobClient = _containerClient.GetBlobClient(fileName);
-   
-   using (var stream = request.Image.OpenReadStream())
+   public DogsController(IConfiguration config, CanineContext context)
    {
-      await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = request.Image.ContentType}, conditions: null);
+      _context = context;
+      _blobConnectionString = config.GetConnectionString("BlobStorage")!;
+      _blobServiceClient = new BlobServiceClient(_blobConnectionString);
+      _containerClient = _blobServiceClient.GetBlobContainerClient("dogblobs");
+
+   }
+   [HttpGet]
+   public async Task<ActionResult<List<Dog>>> Get()
+   {
+      var dogs = await _context.Dogs.ToListAsync();
+      return Ok(dogs);
    }
 
-   var imageResponseUrl = blobClient.Uri.ToString();
+   [HttpPost]
+   public async Task<ActionResult<Dog>> Post([FromForm] DogRequest request)
+   {
+      // Set filename for blob
+      string fileName = request.Name + "_" + Guid.NewGuid().ToString();
+      // Get a reference to a blob
+      var blobClient = _containerClient.GetBlobClient(fileName);
 
-   var newDog = new Dog {
-      Name = request.Name,
-      ImageUrl = imageResponseUrl
-   };
+      using (var stream = request.Image.OpenReadStream())
+      {
+         await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = request.Image.ContentType }, conditions: null);
+      }
 
-   await _context.Dogs.AddAsync(newDog);
-   await _context.SaveChangesAsync();
-    return Ok(newDog);
- }
+      var imageResponseUrl = blobClient.Uri.ToString();
 
-    // DELETE: api/Dog/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteDog(int id)
-    {
-        var dog = await _context.Dogs.FindAsync(id);
-        if (dog == null)
-        {
-            return NotFound();
-        }
+      var newDog = new Dog
+      {
+         Name = request.Name,
+         ImageUrl = imageResponseUrl
+      };
 
-        _context.Dogs.Remove(dog);
-        await _context.SaveChangesAsync();
+      await _context.Dogs.AddAsync(newDog);
+      await _context.SaveChangesAsync();
+      return Ok(newDog);
+   }
 
-        return NoContent();
-    }
+   // DELETE: api/Dog/5
+   [HttpDelete("{id}")]
+   public async Task<IActionResult> DeleteDog(int id)
+   {
+      var dog = await _context.Dogs.FindAsync(id);
+      if (dog == null)
+      {
+         return NotFound();
+      }
+
+      _context.Dogs.Remove(dog);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+   }
 
    //       // PUT: api/Dog/5
    //  // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
